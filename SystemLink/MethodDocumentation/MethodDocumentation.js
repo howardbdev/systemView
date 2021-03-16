2de6066678ee80d56c602efbc4f42d8acd0fd49f
odion100 <odionedwards@gmail.com>
2021-03-16 14:55:10 -0400

    coded MethodDocumentation api and model

--- SystemLink/MethodDocumentation/MethodDocumentation.js
+++ SystemLink/MethodDocumentation/MethodDocumentation.js
@@ -5,19 +5,62 @@ const MethodDocumentationModel = require("./MethodDocumentation.model");
 App.ServerModule("MethodDocumentation", function () {
   const MethodDocumentation = this;
 
-  MethodDocumentation.saveMethodDataTable = (data, cb) => {
-    cb(null, { message: "documentation" });
-  };
+  MethodDocumentation.saveDocs = async (data, cb) => {
+    const {
+      project_code,
+      service_id,
+      module_name,
+      method_name,
+      description,
+      request_data,
+      response_data,
+      triggered_events,
+    } = data;
+    try {
+      const MethodDocumentation = await MethodDocumentationModel.findOne({
+        project_code,
+        service_id,
+        module_name,
+        method_name,
+      });
 
-  MethodDocumentation.saveMethodDescription = (data, cb) => {
-    cb(null, { message: "documentation" });
-  };
+      if (MethodDocumentation) {
+        if (description || description === "") MethodDocumentation.description = description;
+        if (request_data) MethodDocumentation.request_data = request_data;
+        if (response_data) MethodDocumentation.response_data = response_data;
+        if (triggered_events) MethodDocumentation.triggered_events = triggered_events;
+        if (request_data) MethodDocumentation.request_data = request_data;
 
-  MethodDocumentation.saveReturnDataTable = (data, cb) => {
-    cb(null, { message: "documentation" });
-  };
-
-  MethodDocumentation.saveReturnDataDescription = (data, cb) => {
-    cb(null, { message: "documentation" });
+        MethodDocumentation.save()
+          .then((MethodDocumentation) =>
+            cb(null, {
+              MethodDocumentation,
+              status: 200,
+              message: "MethodDocumentation saved succesfully",
+            })
+          )
+          .catch((error) =>
+            cb({ error, status: 400, message: "New MethodDocumentation save attempt failed" })
+          );
+      } else {
+        new MethodDocumentationModel({
+          ...data,
+          _id: Types.ObjectId(),
+        })
+          .save()
+          .then((MethodDocumentation) =>
+            cb(null, {
+              MethodDocumentation,
+              status: 200,
+              message: "New MethodDocumentation saved succesfully",
+            })
+          )
+          .catch((error) =>
+            cb({ error, status: 400, message: "New MethodDocumentation save attempt failed" })
+          );
+      }
+    } catch (error) {
+      cb(error);
+    }
   };
 });
--- SystemLink/MethodDocumentation/MethodDocumentation.model.js
+++ SystemLink/MethodDocumentation/MethodDocumentation.model.js
@@ -2,47 +2,69 @@ const { Schema, model } = require("mongoose");
 const moment = require("moment");
 const required = true;
 const unique = true;
+const data_types = [
+  "String",
+  "Object",
+  "ObjectId",
+  "Number",
+  "Boolean",
+  "Array(ObjectIds)",
+  "Array(Objects)",
+  "Array(Strings)",
+  "Array(Numbers)",
+  "Array(Boolean)",
+  "Array(Misc)",
+];
+const namespace_validator = {
+  validator: (namespace) => namespace.indexOf(" ") === -1,
+  message: `variable namespace cannot contain empty spaces.`,
+};
 
 module.exports = model(
   "MethodDocumentation",
   Schema({
     _id: Schema.Types.ObjectId,
-    project_code: { type: String, required },
+    project_code: { type: String, required, validate: namespace_validator },
+    service_id: { type: String, required, validate: namespace_validator },
+    module_name: { type: String, required, validate: namespace_validator },
+    method_name: { type: String, required, validate: namespace_validator },
     document_ref: { type: String, required },
-    module_name: { type: String, required },
-    method_name: { type: String, required },
+
     description: { type: String },
+
     request_data: {
-      name: { type: String },
-      description: { type: String },
-      type: { type: String },
-      object_ref: { type: String, required },
+      variable_namespace: { type: String, required, validate: namespace_validator },
+      data_type: { type: String, required, enum: data_types },
+      object_ref: { type: String },
       properties: [
         {
-          name: { type: String },
-          type: { type: String },
+          name: { type: String, required, validate: namespace_validator },
+          data_type: { type: String, required, enum: data_types },
+          object_ref: { type: String },
           description: { type: String },
-          default: { type: String },
-          required: { type: String },
+          default_value: { type: String },
+          required: { type: Boolean, default: false },
         },
       ],
     },
+
     response_data: {
-      name: { type: String },
-      description: { type: String },
-      type: { type: String },
-      object_ref: { type: String, required },
+      variable_namespace: { type: String, required, validate: namespace_validator },
+      data_type: { type: String, required, enum: data_types },
+      object_ref: { type: String },
       properties: [
         {
-          name: { type: String },
-          type: { type: String },
+          name: { type: String, required, validate: namespace_validator },
+          data_type: { type: String, required, enum: data_types },
+          object_ref: { type: String },
           description: { type: String },
-          default: { type: String },
-          required: { type: String },
+          default_value: { type: String },
+          required: { type: Boolean, default: false },
         },
       ],
     },
-    emitted_events: [
+
+    triggered_events: [
       {
         name: { type: String, required },
         document_ref: { type: String, required },
