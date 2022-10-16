60099aa45e73ea4bf71b7e9dd722962c6b7bacd8
odion100 <odionedwards@gmail.com>
2022-10-16 09:03:12 -0400

    feat(systemview): setting up method for remote logging

--- SystemView/SystemView/index.js
+++ SystemView/SystemView/index.js
@@ -1,7 +1,8 @@
 const connect = require("./methods/connect");
 const getServices = require("./methods/getServices");
-
+const log = require("./methods/log");
 module.exports = {
   connect,
   getServices,
+  log,
 };
---
+++ SystemView/SystemView/methods/log.js
@@ -0,0 +1,3 @@
+module.exports = async function () {
+  // remote logger
+};
